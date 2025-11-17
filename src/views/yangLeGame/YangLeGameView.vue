7a8a73e2f83ab42e433e1425eb2d86a3ee418215
YH <1191759350@qq.com>
2025-11-17 18:00:43 +0800

    commit

--- src/views/yangLeGame/YangLeGameView.vue
+++ src/views/yangLeGame/YangLeGameView.vue
@@ -1,22 +1,76 @@
 <template>
-  <div>333</div>
+  <div class="yang-le-game">
+    <div
+      v-for="(item, index) in cardArr"
+      :key="index"
+      class="yang-le-game__block"
+      :class="{
+        'yang-le-game__block--isTop': item.isTop,
+      }"
+      :style="{
+        left: (item.x / 2) * 60 + 'px',
+        top: (item.y / 2) * 60 + 'px',
+        zIndex: item.z,
+      }"
+    ></div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { useCardMap } from "./gameLogic/cardMap";
+import { ref } from "vue";
+import { useCardMap, type CardData } from "./gameLogic/cardMap";
 
-useCardMap({
+const cardArr = ref<CardData[]>([]);
+
+cardArr.value = useCardMap({
   mapTemplate: [
-    ["o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o"],
-    ["o", "o", "o", "o", "o", "o", "o"],
-    ["x", "o", "o", "o", "o", "o", "x"],
-    ["x", "o", "o", "o", "o", "o", "x"],
-    ["x", "o", "o", "o", "o", "o", "x"],
-    ["x", "o", "o", "o", "o", "o", "x"],
-    ["x", "o", "o", "o", "o", "o", "x"],
+    // ["o", "o", "o", "o", "o", "o", "o"],
+    // ["o", "o", "o", "o", "o", "o", "o"],
+    // ["o", "o", "o", "o", "o", "o", "o"],
+    // ["x", "o", "o", "o", "o", "o", "x"],
+    // ["x", "o", "o", "o", "o", "o", "x"],
+    // ["x", "o", "o", "o", "o", "o", "x"],
+    // ["x", "o", "o", "o", "o", "o", "x"],
+    // ["x", "o", "o", "o", "o", "o", "x"],
+
+    // ["o", "o", "o"],
+    // ["o", "x", "o"],
+    // ["o", "o", "o"],
+    ["o", "o"],
   ],
   arrangeRule: "random",
-  cardCount: 300,
+  cardCount: 6,
 });
 </script>
+
+<style lang="scss" scoped>
+$block-size: 60px; // 块尺寸
+
+.yang-le-game {
+  position: relative;
+
+  &__block {
+    position: absolute;
+    width: $block-size;
+    height: $block-size;
+    background-color: green;
+    box-shadow: 0 0 5px black;
+
+    &::before {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background-color: rgba(0, 0, 0, 0.5);
+      content: "";
+    }
+
+    &--isTop {
+      &::before {
+        display: none;
+      }
+    }
+  }
+}
+</style>
--- src/views/yangLeGame/gameLogic/cardMap.ts
+++ src/views/yangLeGame/gameLogic/cardMap.ts
@@ -4,53 +4,143 @@ export interface Options {
   cardCount: number; // 卡片数量
 }
 
-export function useCardMap(options: Options) {
-  let cardMap: number[][][];
+export interface CardData {
+  x: number;
+  y: number;
+  z: number;
+  isTop: boolean;
+}
 
+export function useCardMap(options: Options): CardData[] {
+  const { availablePointMap, availablePointArr } = initAvailableZone(
+    options.mapTemplate
+  );
+
+  let cardMap: number[][][] = [];
+  let cardArr: [x: number, y: number, z: number][] = [];
   switch (options.arrangeRule) {
-    case "random":
-      cardMap = randomRule(options);
+    case "random": {
+      const data = randomRule(options, availablePointMap, availablePointArr);
+      cardMap = data.cardMap;
+      cardArr = data.cardArr;
       break;
+    }
   }
+
+  const cardDataList: CardData[] = [];
+  cardMap.forEach((layer, z) => {
+    layer.forEach((row, y) => {
+      row.forEach((cell, x) => {
+        if (cell > -1) {
+          cardDataList.push({
+            x,
+            y,
+            z,
+            isTop: cell === 0,
+          });
+        }
+      });
+    });
+  });
+
+  return cardDataList;
+}
+
+/**
+ * 初始化可用区域
+ * @param mapTemplate map模板
+ * @returns {availablePointMap: ("x" | "o")[][];availablePointArr: [x: number, y: number][];} 可用区域数据
+ */
+function initAvailableZone(mapTemplate: ("x" | "o")[][]): {
+  availablePointMap: ("x" | "o")[][];
+  availablePointArr: [x: number, y: number][];
+} {
+  const availablePointMap: ("x" | "o")[][] = []; // 可用点位map
+  const availablePointArr: [x: number, y: number][] = []; // 可用点位arr
+
+  const setPoint = (x: number, y: number) => {
+    availablePointMap[y][x] = "o";
+    availablePointArr.push([x, y]);
+  }; // 设置可用点位
+
+  mapTemplate.forEach((row, y) => {
+    // 初始化可用点位map数据
+    for (let i = Math.min(1, y); i >= 0; i--) {
+      availablePointMap.push(Array(row.length * 2 - 1).fill("x"));
+    }
+
+    row.forEach((cell, x) => {
+      if (cell !== "o") return;
+      const mY = y * 2;
+      const mX = x * 2;
+      const isTO = mapTemplate[y - 1]?.[x] === "o";
+      const isLO = mapTemplate[y][x - 1] === "o";
+      const isTLO = mapTemplate[y - 1]?.[x - 1] === "o";
+      setPoint(mX, mY);
+      if (isTO) setPoint(mX, mY - 1);
+      if (isLO) setPoint(mX - 1, mY);
+      if (isTO && isLO && isTLO) setPoint(mX - 1, mY - 1);
+    });
+  });
+
+  return { availablePointMap, availablePointArr };
 }
 
 /**
  * 随机分布规则
  * @param options 配置
  */
-function randomRule(options: Options): number[][][] {
+function randomRule(
+  options: Options,
+  availablePointMap: ("x" | "o")[][],
+  availablePointArr: [x: number, y: number][]
+): { cardMap: number[][][]; cardArr: [x: number, y: number, z: number][] } {
   const cardMap: number[][][] = [];
-
-  const availableZone: [x: number, y: number][] = [];
-  options.mapTemplate.forEach((row, y) => {
-    row.forEach((cell, x) => {
-      if (cell === "o") availableZone.push([x, y]);
-    });
-  });
-  const availableZoneCount = availableZone.length;
+  const cardArr: [x: number, y: number, z: number][] = [];
+  const availablePointArrCount = availablePointArr.length;
 
   let count = options.cardCount;
   while (count-- > 0) {
-    const index = Math.floor(Math.random() * availableZoneCount);
-    const [x, y] = availableZone[index];
-    let isDrop = false;
+    const index = Math.floor(Math.random() * availablePointArrCount);
+    const [x, y] = availablePointArr[index];
+    let z = -1;
     for (let i = cardMap.length - 1; i >= 0; i--) {
-      const nextI = i - 1;
-      if (cardMap[i][y][x] <= 0) {
-        cardMap[i][y][x] = 1;
-
-        if (nextI >= 0) {
-          const o = nextI % 2 === 0 ? -1 : 1;
-          if (cardMap[nextI][y][x] === undefined) {
-            console.log("333");
-          }
-        }
-
-        isDrop = true;
-        break;
+      if (
+        cardMap[i][y][x] === -1 &&
+        (cardMap[i][y]?.[x - 1] ?? -1) === -1 &&
+        (cardMap[i][y]?.[x + 1] ?? -1) === -1 &&
+        (cardMap[i][y - 1]?.[x] ?? -1) === -1 &&
+        (cardMap[i][y - 1]?.[x - 1] ?? -1) === -1 &&
+        (cardMap[i][y - 1]?.[x + 1] ?? -1) === -1 &&
+        (cardMap[i][y + 1]?.[x] ?? -1) === -1 &&
+        (cardMap[i][y + 1]?.[x - 1] ?? -1) === -1 &&
+        (cardMap[i][y + 1]?.[x + 1] ?? -1) === -1
+      ) {
+        z = i;
       }
+    }
+
+    if (z === -1) {
+      z = cardMap.length;
+      cardMap[z] = availablePointMap.map((row) => row.map(() => -1));
+    }
+
+    cardMap[z][y][x] = 0;
+    cardArr.push([x, y, z]);
+
+    const nZ = z - 1;
+    if (nZ >= 0) {
+      if (cardMap[nZ][y][x] >= 0) cardMap[nZ][y][x] += 1;
+      if (cardMap[nZ][y]?.[x - 1] >= 0) cardMap[nZ][y][x - 1] += 1;
+      if (cardMap[nZ][y]?.[x + 1] >= 0) cardMap[nZ][y][x + 1] += 1;
+      if (cardMap[nZ][y - 1]?.[x] >= 0) cardMap[nZ][y - 1][x] += 1;
+      if (cardMap[nZ][y - 1]?.[x - 1] >= 0) cardMap[nZ][y - 1][x - 1] += 1;
+      if (cardMap[nZ][y - 1]?.[x + 1] >= 0) cardMap[nZ][y - 1][x + 1] += 1;
+      if (cardMap[nZ][y + 1]?.[x] >= 0) cardMap[nZ][y + 1][x] += 1;
+      if (cardMap[nZ][y + 1]?.[x - 1] >= 0) cardMap[nZ][y + 1][x - 1] += 1;
+      if (cardMap[nZ][y + 1]?.[x + 1] >= 0) cardMap[nZ][y + 1][x + 1] += 1;
     }
   }
 
-  return cardMap;
+  return { cardMap, cardArr };
 }
